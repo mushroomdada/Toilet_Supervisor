b128a145af3bad7d08d44df8fccc21cb3bb2a965
mushroomdada <553474661@qq.com>
2016-07-12 16:50:42 +0800

    生成精灵、动作、调度器刷新变色

--- Classes/gameScene.cpp
+++ Classes/gameScene.cpp
@@ -91,7 +91,6 @@ bool gameScene::init()
 	addChild(destination2woman);
 	addChild(destination2man);
 
-
 	//a menu to count to begin the game
 	middleMenu = Menu::create();
 	middleMenu->setAnchorPoint(Vec2::ANCHOR_MIDDLE);
@@ -100,11 +99,103 @@ bool gameScene::init()
 
 	//add 15 characters into the vector
 	characterQuantity = 15;
-	
-	generateRandomNumForArrays();
+	thisCharacterNum = 0;
 
+	isCharacterExist = false;
+	gameWin = false;
+	gameLose = false;
+
+	generateRandomNumForArrays();
+	schedule(schedule_selector(gameScene::update));
 
 	return true;
+}
+
+
+void gameScene::update(float dt)
+{
+	if (isCharacterExist) {
+		if (thisCharacter->getPosition() == Vec2(winSize.width / 2, origin.y)) {
+			if (characterTypeArray[thisCharacterNum] == 1) {
+				middleMenu->removeAllChildren();
+				labelLose = Label::createWithTTF("You Lose!", "fonts/Marker Felt.ttf", COUNT_DOWN_FONT_SIZE);
+				itemLose = MenuItemLabel::create(labelLose);
+				middleMenu->addChild(itemLose);
+				isCharacterExist = false;
+				gameLose = true;
+				playEffectFail();
+				Director::getInstance()->pause();
+			}
+		}
+		if (thisCharacter->getPosition() == Vec2(winSize.width / 2, origin.y - thisCharacter->getContentSize().height)) {
+			thisCharacter->removeFromParentAndCleanup(true);
+			isCharacterExist = false;
+
+			bool changemale = random(2);
+			if (changemale) {
+				changeDestination();
+			}
+			//dispatcher->removeEventListener(listenerkeyPad);
+		}
+	}
+	if (isCharacterExist) {
+		if (thisCharacter->getPosition().y <= winSize.height / 2 + 5 && thisCharacter->getPosition().y >= winSize.height / 2 - 5) {
+			//set the begin time
+			thisCharacterBeginTime = getCurrentTime();
+
+			//change character color
+			changeCharacterColor();
+		}
+	}
+}
+
+long gameScene::getCurrentTime()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+void gameScene::changeDestination()
+{
+	if (destinaionType == 1) {
+		destinaionType = 2;
+		destination1man->setVisible(false);
+		destination1woman->setVisible(true);
+		destination2man->setVisible(true);
+		destination2woman->setVisible(false);
+	}
+	else if (destinaionType == 2) {
+		destinaionType = 1;
+		destination1man->setVisible(true);
+		destination1woman->setVisible(false);
+		destination2man->setVisible(false);
+		destination2woman->setVisible(true);
+	}
+}
+
+void gameScene::changeCharacterColor()
+{
+	if (manOrWomanArray[thisCharacterNum] == 1) {
+		if (characterTypeArray[thisCharacterNum] == 1) {
+			SpriteFrame* a = SpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("manRed1.png");
+			thisCharacter->setDisplayFrame(a);
+		}
+		else {
+			SpriteFrame* a = SpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("manGreen1.png");
+			thisCharacter->setDisplayFrame(a);
+		}
+	}
+	else {
+		if (characterTypeArray[thisCharacterNum] == 1) {
+			SpriteFrame* a = SpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("womanRed1.png");
+			thisCharacter->setDisplayFrame(a);
+		}
+		else {
+			SpriteFrame* a = SpriteFrameCache::sharedSpriteFrameCache()->spriteFrameByName("womanGreen1.png");
+			thisCharacter->setDisplayFrame(a);
+		}
+	}
 }
 
 void gameScene::generateRandomNumForArrays()
@@ -159,5 +250,154 @@ void gameScene::stopBgm()
 }
 
 
+
+
+
+void gameScene::gameBegin(float dt)
+{
+	//initialization
+	middleMenu->removeAllChildren();
+	isCharacterExist = false;
+	gameLose = false;
+	gameWin = false;
+	//game begin
+	destinaionType = 1;
+	thisCharacterNum = -1;
+
+	//call createCharacter function to begin the characters action
+	timeBetweenDestinations = 3.0f;
+
+
+	//schedule(schedule_selector(gameScene::changeDestination), timeBetweenDestinations);
+
+
+	scheduleOnce(schedule_selector(gameScene::runCharacterA), 0.0);
+}
+
+
+
+
+void gameScene::runCharacterA(float dt)
+{
+	thisCharacterNum++;
+	//change destination exchanging frequency
+	//unschedule(schedule_selector(gameScene::changeDestination));
+	if (timeBetweenDestinations > 1.5f) {
+		timeBetweenDestinations -= 0.15f;
+	}
+	//schedule(schedule_selector(gameScene::changeDestination), timeBetweenDestinations);
+	if (!gameWin && !gameLose && thisCharacterNum < characterQuantity)
+	{
+		middleMenu->removeAllChildren();
+
+		std::string characterType = (manOrWomanArray[thisCharacterNum] == 1) ? "manBlack1.png" : "womanBlack1.png";
+		thisCharacter = Sprite::createWithSpriteFrame(cache->spriteFrameByName(characterType));
+
+		thisCharacter->setAnchorPoint(Vec2::ANCHOR_MIDDLE_BOTTOM);
+		thisCharacter->setPosition(winSize.width / 2, winSize.height);
+		addChild(thisCharacter);
+		isCharacterExist = true;
+		//action move
+		if (characterTypeArray[thisCharacterNum] == 1) {
+			auto actionMove = MoveTo::create(timeBetweenDestinations, Vec2(winSize.width / 2, origin.y));
+			thisCharacter->runAction(actionMove);
+		}
+		else {
+			auto actionMove = MoveTo::create(timeBetweenDestinations, Vec2(winSize.width / 2, origin.y - thisCharacter->getContentSize().height));
+			thisCharacter->runAction(actionMove);
+		}
+
+		scheduleOnce(schedule_selector(gameScene::runCharacterB), 4.0);
+	}
+	else if (thisCharacterNum == characterQuantity) {
+		isCharacterExist = false;
+		middleMenu->removeAllChildren();
+		//label of win
+		auto labelWin = Label::createWithTTF("You win!", "fonts/Marker Felt.ttf", COUNT_DOWN_FONT_SIZE);
+		auto itemWin = MenuItemLabel::create(labelWin);
+		itemWin->setPosition(0, 30);
+		//label of time
+		int tempTimeInt = playerGameTime;
+		std::string tempTimeString = Value(tempTimeInt).asString();
+		int tempTimeStringLength = tempTimeString.length();
+		std::string tempTimeStringAfterConverted = "";
+		for (int i = 0; i < tempTimeStringLength; i++) {
+			if (i == tempTimeStringLength - 3) {
+				tempTimeStringAfterConverted += '.';
+			}
+			tempTimeStringAfterConverted += tempTimeString[i];
+		}
+		auto labelTime = Label::createWithTTF(tempTimeStringAfterConverted, "fonts/Marker Felt.ttf", COUNT_DOWN_FONT_SIZE);
+		auto itemTime = MenuItemLabel::create(labelTime);
+		itemTime->setPosition(0, -30);
+		middleMenu->addChild(itemWin);
+		middleMenu->addChild(itemTime);
+		gameLose = false;
+		gameWin = true;
+		this->unscheduleAllSelectors();
+	}
+}
+
+void gameScene::runCharacterB(float dt)
+{
+	thisCharacterNum++;
+	//change destination exchanging frequency
+	//unschedule(schedule_selector(gameScene::changeDestination));
+	if (timeBetweenDestinations > 1.5f) {
+		timeBetweenDestinations -= 0.15f;
+	}
+	//schedule(schedule_selector(gameScene::changeDestination), timeBetweenDestinations);
+	if (!gameWin && !gameLose && thisCharacterNum < characterQuantity)
+	{
+		middleMenu->removeAllChildren();
+
+		std::string characterType = (manOrWomanArray[thisCharacterNum] == 1) ? "manBlack1.png" : "womanBlack1.png";
+		thisCharacter = Sprite::createWithSpriteFrame(cache->spriteFrameByName(characterType));
+
+		thisCharacter->setAnchorPoint(Vec2::ANCHOR_MIDDLE_BOTTOM);
+		thisCharacter->setPosition(winSize.width / 2, winSize.height);
+		addChild(thisCharacter);
+		isCharacterExist = true;
+
+		if (characterTypeArray[thisCharacterNum] == 1) {
+			auto actionMove = MoveTo::create(timeBetweenDestinations, Vec2(winSize.width / 2, origin.y));
+			thisCharacter->runAction(actionMove);
+		}
+		else {
+			auto actionMove = MoveTo::create(timeBetweenDestinations, Vec2(winSize.width / 2, origin.y - thisCharacter->getContentSize().height));
+			thisCharacter->runAction(actionMove);
+		}
+		scheduleOnce(schedule_selector(gameScene::runCharacterA), 4.0);
+
+	}
+	else if (thisCharacterNum == characterQuantity) {
+		isCharacterExist = false;
+		middleMenu->removeAllChildren();
+		auto labelWin = Label::createWithTTF("You win!", "fonts/Marker Felt.ttf", COUNT_DOWN_FONT_SIZE);
+		auto itemWin = MenuItemLabel::create(labelWin);
+		itemWin->setPosition(0, 30);
+		int tempTime = playerGameTime;
+		//label of time
+		int tempTimeInt = playerGameTime;
+		std::string tempTimeString = Value(tempTimeInt).asString();
+		int tempTimeStringLength = tempTimeString.length();
+		std::string tempTimeStringAfterConverted = "\nYour Time:\n";
+		for (int i = 0; i < tempTimeStringLength; i++) {
+			if (i == tempTimeStringLength - 3) {
+				tempTimeStringAfterConverted += '.';
+			}
+			tempTimeStringAfterConverted += tempTimeString[i];
+		}
+		tempTimeStringAfterConverted += "s";
+		auto labelTime = Label::createWithTTF(tempTimeStringAfterConverted, "fonts/Marker Felt.ttf", 40);
+		auto itemTime = MenuItemLabel::create(labelTime);
+		itemTime->setPosition(0, -30);
+		middleMenu->addChild(itemWin);
+		middleMenu->addChild(itemTime);
+		gameLose = false;
+		gameWin = true;
+		this->unscheduleAllSelectors();
+	}
+}
 
 
--- Classes/gameScene.h
+++ Classes/gameScene.h
@@ -10,8 +10,20 @@ public:
 	static Scene* createScene();
 	static gameScene* create();
 	bool init();
-	//void update(float);
+	void update(float);
+	long getCurrentTime();
+	void changeCharacterColor();
 	void generateRandomNumForArrays();
+
+
+	void gameBegin(float);
+
+	void runCharacterA(float);
+	void runCharacterB(float);
+
+	void onKeyPressed(EventKeyboard::KeyCode keycode, cocos2d::Event *event);
+
+	void changeDestination();
 
 	void preloadMusic();
 	void playBgm();
@@ -25,20 +37,32 @@ private:
 	Size winSize = Director::getInstance()->getWinSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 	EventDispatcher* dispatcher = Director::getInstance()->getEventDispatcher();
-	//EventListenerKeyboard* listenerkeyPad;
 	SpriteFrameCache* cache;
 
 	Vec2 leftDestination = Vec2(winSize.width / 3 / 2, winSize.height / 5);
 	Vec2 rightDestination = Vec2(winSize.width / 6 * 5, winSize.height / 5);
 
 	Menu* middleMenu;
-	Menu* rightTopMenu;
+	Label* labelLose;
+	MenuItemLabel* itemLose;
+
+	bool gameWin;
+	bool gameLose;
+	bool isCharacterExist;
+
+	Sprite* thisCharacter;
 	int manOrWomanArray[15];
 	int characterTypeArray[15];
 	int characterQuantity;
+	int thisCharacterNum;
+
+	long thisCharacterBeginTime = 0;
+	long playerGameTime = 0;
+
 	Sprite* destination1man;
 	Sprite* destination1woman;
 	Sprite* destination2man;
 	Sprite* destination2woman;
 	int destinaionType;
+	float timeBetweenDestinations;
 };
